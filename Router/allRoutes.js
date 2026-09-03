b59911ab39d3bdfc2ddccf73ae379821538e225b
projet2506 <projet2506@outlook.fr>
2026-09-03 09:15:26 +0200

    correction

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,9 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
-
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/Galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
+    
+];
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
